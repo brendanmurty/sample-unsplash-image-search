27c1898053fded3d7a8ea8931c3fc2775775e400
Brendan Murty <brendan@murty.au>
2023-02-16 13:52:34 +1100

    Component ListImages: Improve grid layout

--- src/components/ListImages/ListImages.js
+++ src/components/ListImages/ListImages.js
@@ -14,21 +14,23 @@ class ListImages extends Component {
   render() {
     if (this.props.images) {
       return (
-        <ul className="ListImages-list">
-          {this.props.images.map(image => (
-          <li key={image.links.html.toString() }>
-            <a href={image.links.html}>
-              <span className="ListImages-item-image">
-                {image.description ? (
-                <img src={image.urls.small} alt={image.description} title={image.user.name + ' - ' + image.description } />
-                ) : (
-                <img src={image.urls.small} alt={'Image by ' + image.user.name} title={'Image by ' + image.user.name} />
-                )}
-              </span>
-            </a>
-          </li>
-          ))}
-        </ul>
+        <div className="ListImages-container">
+          <ul className="ListImages-list">
+            {this.props.images.map(image => (
+            <li key={image.links.html.toString() }>
+              <a href={image.links.html}>
+                <span className="ListImages-item-image">
+                  {image.description ? (
+                  <img src={image.urls.small} alt={image.description} title={image.user.name + ' - ' + image.description } />
+                  ) : (
+                  <img src={image.urls.small} alt={'Image by ' + image.user.name} title={'Image by ' + image.user.name} />
+                  )}
+                </span>
+              </a>
+            </li>
+            ))}
+          </ul>
+        </div>
       );
     }
   }
