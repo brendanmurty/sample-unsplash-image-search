229a3b1a5044a385e4906a1c8702e9d7a9bb96cb
Brendan Murty <brendan@murty.au>
2023-02-16 14:07:25 +1100

    Component UnsplashApiRequest: Add to docs

--- src/common/UnsplashAPIRequest.js
+++ src/common/UnsplashAPIRequest.js
@@ -9,6 +9,9 @@
  * which would allow the system to provide user-specific customisation options 
  * or allow them to filter photo results by just the photos they've uploaded.
  * 
+ * This should also handle the situation where the API returns a rate limiting
+ * or error state.
+ * 
  * Related Unsplash API documentation: https://unsplash.com/documentation
  * 
  **/
